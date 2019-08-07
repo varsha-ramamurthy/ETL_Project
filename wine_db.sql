817d1b6e38163049f251864ab6457ef0d31877f4
varsha-ramamurthy <50121685+varsha-ramamurthy@users.noreply.github.com>
2019-08-07 14:38:17 -0400

    Wine database sql file
    SQL database storing the wine reviews dataframes obtained from data.world and kaggle, and merged and cleaned using pandas

---
+++ wine_db.sql
@@ -0,0 +1,31 @@
+-- Database: wine_db
+
+-- DROP DATABASE wine_db;
+
+CREATE DATABASE wine_db
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+--Drop table as needed to make edits	
+DROP TABLE wine_table;
+
+--Create tables
+CREATE TABLE wine_table (
+  country VARCHAR(30) NOT NULL,
+  designation VARCHAR(100) NOT NULL,
+  points int NOT NULL,
+  price DECIMAL NOT NULL,
+  province VARCHAR(50) NOT NULL,
+  region VARCHAR(100) NOT NULL,
+  taster_name VARCHAR(100) NOT NULL,
+  title VARCHAR NOT NULL,
+  variety VARCHAR(50) NOT NULL,
+  winery VARCHAR(50) NOT NULL,
+  vintage DATE NOT NULL,
+  county VARCHAR(100) NOT NULL
+  );
